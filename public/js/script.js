35f30500cc2ecaef6bf43bd8acee301e79628500
Arbitermartin <arbitermartin04@gmail.com>
2025-06-05 11:26:04 +0300

    add latest changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,16 @@
-   // Password toggle functionality
+
+// TOGGLE BUTTON 
+// document.getElementById('toggleButton').addEventListener('click', function () {
+//   let ul = document.getElementById('ul');
+//   if (ul.classList.contains('show')) {
+//     this.innerHTML = '<i class="fa fa-bars"></i>';
+//   } else {
+//     this.innerHTML = '<i class="fa fa-x"></i>';
+//   }
+//   ul.classList.toggle('show');
+// });
+
+// Password toggle functionality
    const togglePassword = document.getElementById('togglePassword');
    const passwordInput = document.getElementById('account_password');
    togglePassword.addEventListener('click', function() {
